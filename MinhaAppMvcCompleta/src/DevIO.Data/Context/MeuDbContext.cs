dab9ac0504ba500151223cfffad8004b50602692
Anderson <anderson@prodwin.com.br>
2020-05-22 21:50:49 -0300

    01 - Mapeando as entidades do banco / Contexto
    Mapeando as entidades do banco com Fluenty Api,
    Criado contexto e configuração do mesmo no Startup.

---
+++ MinhaAppMvcCompleta/src/DevIO.Data/Context/MeuDbContext.cs
@@ -0,0 +1,36 @@
+﻿using DevIO.Business.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace DevIO.Data.Context
+{
+	public class MeuDbContext : DbContext
+	{
+		public MeuDbContext(DbContextOptions options) : base(options) {
+		}
+
+		public DbSet<Produto> Produtos { get; set; }
+		public DbSet<Endereco> Enderecos { get; set; }
+		public DbSet<Fornecedor> Fornecedores { get; set; }
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder) {
+
+			/*Caso não for mapeado alguma propriedade, aqui é setado como default o tipo dela*/
+			foreach(var property in modelBuilder.Model.GetEntityTypes()
+											.SelectMany(e => e.GetProperties()
+											.Where(p => p.ClrType == typeof(string)))) {
+				property.SetColumnType("varchar(100)");
+			}
+
+			modelBuilder.ApplyConfigurationsFromAssembly(typeof(MeuDbContext).Assembly);
+
+			//Aqui estou retirando o delete cascade das entidades
+			foreach(var  item in modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys())) {
+				item.DeleteBehavior = DeleteBehavior.ClientSetNull;
+			}
+
+			base.OnModelCreating(modelBuilder);
+		}
+
+	}
+}
---
+++ MinhaAppMvcCompleta/src/DevIO.Data/Mappings/FornecedorMapping.cs
@@ -0,0 +1,37 @@
+﻿using DevIO.Business.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DevIO.Data.Mappings
+{
+	public class FornecedorMapping: IEntityTypeConfiguration<Fornecedor>
+	{
+		public void Configure(EntityTypeBuilder<Fornecedor> builder) {
+
+			builder.HasKey(p => p.Id);
+			builder.Property(x => x.Nome)
+				.IsRequired()
+				.HasColumnType("varchar(200)");
+
+			builder.Property(x => x.Documento)
+				.IsRequired()
+				.HasColumnType("varchar(14)");
+
+			/*Configuracao 1 > 1 => Fornecedor : Endereco
+			 * Fornecedor tem um endereço
+			 * e Endereço tem um fornecedor (WithOne)*/
+			builder.HasOne(f => f.Endereco)
+				.WithOne(e => e.Fornecedor);
+
+			/*Configuracao 1 > N => Fornecedor : Produtos
+			 * Fornecedor tem varios produtos
+			 * e Produto tem um fornecedor (WithOne)*/
+			builder.HasMany(f => f.Produtos)
+				.WithOne(p => p.Fornecedor)
+				.HasForeignKey(p => p.FornecedorId); // Qual a chave estrangeira entre produto e fornecedor
+
+			builder.ToTable("Fornecedores");
+		}
+	}
+
+}
