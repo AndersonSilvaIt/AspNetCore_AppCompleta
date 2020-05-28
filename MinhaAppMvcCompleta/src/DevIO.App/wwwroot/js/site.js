42fcea1dd48e09191f2a551be4b70057b07c15ad
Anderson <anderson@prodwin.com.br>
2020-05-28 20:10:29 -0300

    11 - Utilizando AJax para preenchimento do CEP
    11 - Utilizando AJax para preenchimento do CEP

--- MinhaAppMvcCompleta/src/DevIO.App/wwwroot/js/site.js
+++ MinhaAppMvcCompleta/src/DevIO.App/wwwroot/js/site.js
@@ -41,3 +41,61 @@
 		}
 	});
 }
+
+function BuscaCEP() {
+	$(document).ready(function () {
+
+		function limpar_formulario_cep() {
+			//Limpa valores do formulário do cep
+			$("#Endereco_Logradouro").val("");
+			$("#Endereco_Bairro").val("");
+			$("#Endereco_Cidade").val("");
+			$("#Endereco_Estado").val("");
+		}
+
+		//Quando o campo cep perde o foco.
+		$("#Endereco_Cep").blur(function () {
+
+			//nova variavel "cep" somente com digitos
+			var cep = $(this).val().replace(/\D/g, '');
+
+			//verifica se campo possui valor informado.
+			if (cep != "") {
+
+				//expressao regular para validar o CEP
+				var validacep = /^[0-9]{8}$/;
+				if (validacep.test(cep)) {
+					$("#Endereco_Logradouro").val("...");
+					$("#Endereco_Bairro").val("...");
+					$("#Endereco_Cidade").val("...");
+					$("#Endereco_Estado").val("...");
+
+					//Consulta o webservice viacep.com.br
+					$.getJSON("https://viacep.com.br/ws/" + cep + "/json/?callback=?",
+						function (dados) {
+							if (!("erro" in dados)) {
+								//Atualiza os campos com os valores da consulta
+								$("#Endereco_Logradouro").val(dados.logradouro);
+								$("#Endereco_Bairro").val(dados.bairro);
+								$("#Endereco_Cidade").val(dados.localidade);
+								$("#Endereco_Estado").val(dados.uf);
+							} //end if
+							else {
+								limpar_formulario_cep();
+								alert("CEP não encontrado");
+							}
+						});
+				} // end if
+				else {
+					//cep é inválido
+					limpar_formulario_cep();
+					alert("Formato de CEP inválido");
+				}
+			} //endif
+			else {
+				//cep sem valor, limpa formulario
+				limpar_formulario_cep();
+			}
+		});
+	});
+}
