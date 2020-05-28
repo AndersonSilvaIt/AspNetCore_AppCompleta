e50b0a656fb63ecd37f64c54ef922ff7fcfc1ab4
Anderson <anderson@prodwin.com.br>
2020-05-28 19:45:44 -0300

    10 - Implementando Modal de Edição
    Implementado dela de Modal Dialog para edição de endereço no cadastro de fornecedor.

--- MinhaAppMvcCompleta/src/DevIO.App/Controllers/FornecedoresController.cs
+++ MinhaAppMvcCompleta/src/DevIO.App/Controllers/FornecedoresController.cs
@@ -9,109 +9,135 @@ using DevIO.Business.Models;
 
 namespace DevIO.App.Controllers
 {
-    public class FornecedoresController : BaseController
-    {
-        private readonly IFornecedorRepository _fornecedorRepository;
-        private readonly IMapper _mapper;
+	public class FornecedoresController: BaseController
+	{
+		private readonly IFornecedorRepository _fornecedorRepository;
+		private readonly IEnderecoRepository _enderecoRepository;
+		private readonly IMapper _mapper;
 
-        public FornecedoresController(IFornecedorRepository fornecedorRepository,
-                                       IMapper mapper)
-        {
-            _fornecedorRepository = fornecedorRepository;
-            _mapper = mapper;
-        }
+		public FornecedoresController(IFornecedorRepository fornecedorRepository,
+									   IMapper mapper,
+									   IEnderecoRepository enderecoRepository) {
+			_fornecedorRepository = fornecedorRepository;
+			_mapper = mapper;
+			_enderecoRepository = enderecoRepository;
+		}
 
-        public async Task<IActionResult> Index()
-        {
-            //Realizado mapeamento dos dados que vem do banco, para a entidade ViewModel que vai para a View
-            return View(_mapper.Map<IEnumerable<FornecedorViewModel>>(await _fornecedorRepository.ObterTodos()));
-        }
+		public async Task<IActionResult> Index() {
+			//Realizado mapeamento dos dados que vem do banco, para a entidade ViewModel que vai para a View
+			return View(_mapper.Map<IEnumerable<FornecedorViewModel>>(await _fornecedorRepository.ObterTodos()));
+		}
 
-        public async Task<IActionResult> Details(Guid id)
-        {
-            var fornecedorViewModel = await ObterFornecedorEndereco(id);
+		public async Task<IActionResult> Details(Guid id) {
+			var fornecedorViewModel = await ObterFornecedorEndereco(id);
 
-            if (fornecedorViewModel == null)
-            {
-                return NotFound();
-            }
+			if(fornecedorViewModel == null) {
+				return NotFound();
+			}
 
-            return View(fornecedorViewModel);
-        }
+			return View(fornecedorViewModel);
+		}
 
-        public IActionResult Create()
-        {
-            return View();
-        }
+		public IActionResult Create() {
+			return View();
+		}
 
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create(FornecedorViewModel fornecedorViewModel)
-        {
-            if (!ModelState.IsValid)  return View(fornecedorViewModel);
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> Create(FornecedorViewModel fornecedorViewModel) {
+			if(!ModelState.IsValid) return View(fornecedorViewModel);
 
-            var fornecedor = _mapper.Map<Fornecedor>(fornecedorViewModel);
-            await _fornecedorRepository.Adicionar(fornecedor);
+			var fornecedor = _mapper.Map<Fornecedor>(fornecedorViewModel);
+			await _fornecedorRepository.Adicionar(fornecedor);
 
-            return RedirectToAction("Index");
-        }
+			return RedirectToAction("Index");
+		}
 
-        public async Task<IActionResult> Edit(Guid id)
-        {
-            var fornecedorViewModel = await ObterFornecedorProduosEndereco(id);
+		public async Task<IActionResult> Edit(Guid id) {
+			var fornecedorViewModel = await ObterFornecedorProduosEndereco(id);
 
-            if (fornecedorViewModel == null)
-                return NotFound();
+			if(fornecedorViewModel == null)
+				return NotFound();
 
-            return View(fornecedorViewModel);
-        }
+			return View(fornecedorViewModel);
+		}
 
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(Guid id, FornecedorViewModel fornecedorViewModel)
-        {
-            if (id != fornecedorViewModel.Id) return NotFound();
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> Edit(Guid id, FornecedorViewModel fornecedorViewModel) {
+			if(id != fornecedorViewModel.Id) return NotFound();
 
-            if (!ModelState.IsValid)
-                return View(fornecedorViewModel);
+			if(!ModelState.IsValid)
+				return View(fornecedorViewModel);
 
-            var fornecedor = _mapper.Map<Fornecedor>(fornecedorViewModel);
-            await _fornecedorRepository.Atualizar(fornecedor);
+			var fornecedor = _mapper.Map<Fornecedor>(fornecedorViewModel);
+			await _fornecedorRepository.Atualizar(fornecedor);
 
-            return RedirectToAction("Index");
-        }
-        
-        public async Task<IActionResult> Delete(Guid id)
-        {
-            var fornecedorViewModel = await ObterFornecedorEndereco(id);
+			return RedirectToAction("Index");
+		}
 
-            if (fornecedorViewModel == null) return NotFound();
+		public async Task<IActionResult> Delete(Guid id) {
+			var fornecedorViewModel = await ObterFornecedorEndereco(id);
 
-            return View(fornecedorViewModel);
-        }
+			if(fornecedorViewModel == null) return NotFound();
 
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(Guid id)
-        {
-            var fornecedorViewModel = await ObterFornecedorEndereco(id);
+			return View(fornecedorViewModel);
+		}
 
-            if(fornecedorViewModel == null)
-                return NotFound();
+		[HttpPost, ActionName("Delete")]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> DeleteConfirmed(Guid id) {
+			var fornecedorViewModel = await ObterFornecedorEndereco(id);
 
-            await _fornecedorRepository.Remover(id);
+			if(fornecedorViewModel == null)
+				return NotFound();
 
-            return RedirectToAction("Index");
-        }
+			await _fornecedorRepository.Remover(id);
 
-        private async Task<FornecedorViewModel> ObterFornecedorEndereco(Guid id) {
+			return RedirectToAction("Index");
+		}
 
-         return  _mapper.Map<FornecedorViewModel>(await _fornecedorRepository.ObterFornecedorEndereco(id));
-        }
+		public async Task<IActionResult> AtualizarEndereco(Guid id) {
+			var fornecedor = await ObterFornecedorEndereco(id);
+			if(fornecedor == null) {
+				return NotFound();
+			}
+			return PartialView("_AtualizarEndereco", new FornecedorViewModel { Endereco = fornecedor.Endereco });
+		}
 
-        private async Task<FornecedorViewModel> ObterFornecedorProduosEndereco(Guid id) {
+		public async Task<IActionResult> ObterEndereco(Guid id) {
+			
+			var fornecedor = await ObterFornecedorEndereco(id);
+			if(fornecedor == null)
+				return NotFound();
 
-            return _mapper.Map<FornecedorViewModel>(await _fornecedorRepository.ObterFornecedorProdutoEndereco(id));
-        }
-    }
+			return PartialView("_DetalhesEndereco", fornecedor); // ele vai atualizar somente a partial view _DetalhesEndereco que está no edit
+		}
+
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> AtualizarEndereco(FornecedorViewModel fornecedorViewModel) {
+
+			ModelState.Remove("Nome");
+			ModelState.Remove("Documento");
+
+			if(!ModelState.IsValid) 
+				return PartialView("_AtualizarEndereco", fornecedorViewModel);
+
+			await _enderecoRepository.Atualizar(_mapper.Map<Endereco>(fornecedorViewModel.Endereco));
+
+			var url = Url.Action("ObterEndereco", "Fornecedores", new { id = fornecedorViewModel.Endereco.FornecedorId });
+			return Json(new { success = true, url });
+		}
+
+		private async Task<FornecedorViewModel> ObterFornecedorEndereco(Guid id) {
+
+			return _mapper.Map<FornecedorViewModel>(await _fornecedorRepository.ObterFornecedorEndereco(id));
+		}
+
+		private async Task<FornecedorViewModel> ObterFornecedorProduosEndereco(Guid id) {
+
+			return _mapper.Map<FornecedorViewModel>(await _fornecedorRepository.ObterFornecedorProdutoEndereco(id));
+		}
+	}
 }
