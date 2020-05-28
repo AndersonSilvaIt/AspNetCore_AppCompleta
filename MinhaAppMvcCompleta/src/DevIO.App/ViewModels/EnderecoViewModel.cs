6757418b728d88af81361e3f1988c58384d9eefc
Anderson <anderson@prodwin.com.br>
2020-05-28 17:53:34 -0300

    Implementando tratamento nas views de detalhes, exlusão e etc.
    Implementando tratamento nas views de detalhes, exlusão e etc.

--- MinhaAppMvcCompleta/src/DevIO.App/ViewModels/EnderecoViewModel.cs
+++ MinhaAppMvcCompleta/src/DevIO.App/ViewModels/EnderecoViewModel.cs
@@ -14,7 +14,7 @@ namespace DevIO.App.ViewModels
 		public string Logradouro { get; set; }
 
 		[Required(ErrorMessage = "O campo {0} é obrigatório")]
-		[StringLength(50, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 2)]
+		[StringLength(50, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 1)]
 		public string Numero { get; set; }
 
 		public string Complemento { get; set; }
@@ -28,7 +28,7 @@ namespace DevIO.App.ViewModels
 		public string Cep { get; set; }
 
 		[Required(ErrorMessage = "O campo {0} é obrigatório")]
-		[StringLength(100, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 8)]
+		[StringLength(100, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 2)]
 		public string Cidade { get; set; }
 
 		[Required(ErrorMessage = "O campo {0} é obrigatório")]
