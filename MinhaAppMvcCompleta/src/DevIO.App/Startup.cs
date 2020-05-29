20ca10aa3084975b0222fee7d13b4e9c223ea995
Anderson <anderson@prodwin.com.br>
2020-05-28 21:21:49 -0300

    12 - Organizando e Otimizando a estrutura do App
    12 - Organizando e Otimizando a estrutura do App

--- MinhaAppMvcCompleta/src/DevIO.App/Startup.cs
+++ MinhaAppMvcCompleta/src/DevIO.App/Startup.cs
@@ -1,77 +1,61 @@
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
-using DevIO.App.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using DevIO.Data.Context;
-using DevIO.Business.Interfaces;
-using DevIO.Data.Repository;
 using AutoMapper;
-using System.Globalization;
-using Microsoft.AspNetCore.Localization;
-using System.Collections.Generic;
-using Microsoft.AspNetCore.Mvc.DataAnnotations;
-using DevIO.App.Extensions;
+using DevIO.App.Configurations;
 
 namespace DevIO.App
 {
 	public class Startup
 	{
-		public Startup(IConfiguration configuration) {
-			Configuration = configuration;
+		public IConfiguration Configuration { get; }
+
+		public Startup(IWebHostEnvironment hostingEnvironment) {
+
+			var builder = new ConfigurationBuilder()
+				.SetBasePath(hostingEnvironment.ContentRootPath)
+				.AddJsonFile("appsettings.json", true, true)
+				.AddJsonFile($"appsettings.{hostingEnvironment.EnvironmentName}.json", true, true)
+				.AddEnvironmentVariables();
+
+			if(hostingEnvironment.IsDevelopment()) {
+				builder.AddUserSecrets<Startup>();
+			}
+
+			Configuration = builder.Build();
 		}
 
-		public IConfiguration Configuration { get; }
-		// This method gets called by the runtime. Use this method to add services to the container.
+		//public Startup(IConfiguration configuration) {
+		//	Configuration = configuration;
+		//}
+
+		
 		public void ConfigureServices(IServiceCollection services) {
-			services.AddDbContext<ApplicationDbContext>(options =>
-				options.UseSqlServer( Configuration.GetConnectionString("DefaultConnection")));
+
+			services.AddIdentityConfiguration(Configuration);
 
 			services.AddDbContext<MeuDbContext>(options =>
 				options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
-
-			services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-				.AddEntityFrameworkStores<ApplicationDbContext>();
 
 			services.AddControllersWithViews();
 			services.AddRazorPages();
 
 			services.AddAutoMapper(typeof(Startup));
 
-			services.AddMvc(options => {
-				options.ModelBindingMessageProvider.SetAttemptedValueIsInvalidAccessor((x, y) => "O valor preenchido é inválido para este campo.");
-				options.ModelBindingMessageProvider.SetMissingBindRequiredValueAccessor(x => "Este campo precisa ser preenchido.");
-				options.ModelBindingMessageProvider.SetMissingKeyOrValueAccessor(() => "Este campo precisa ser preenchido.");
-				options.ModelBindingMessageProvider.SetMissingRequestBodyRequiredValueAccessor(() => "É necessário que o body na requisição não esteja vazio.");
-				options.ModelBindingMessageProvider.SetNonPropertyAttemptedValueIsInvalidAccessor((x) => "O valor preenchidoé inválido para teste campo.");
-				options.ModelBindingMessageProvider.SetNonPropertyUnknownValueIsInvalidAccessor(() => "O valor preenchidoé inválido para teste campo.");
-				options.ModelBindingMessageProvider.SetNonPropertyValueMustBeANumberAccessor(() => "O campo deve ser numérico.");
-				options.ModelBindingMessageProvider.SetUnknownValueIsInvalidAccessor((x) => "O valor preenchido é inválido para este campo.");
-				options.ModelBindingMessageProvider.SetValueIsInvalidAccessor((x) => "O valor preenchido é inválido para este campo.");
-				options.ModelBindingMessageProvider.SetValueMustBeANumberAccessor(x => "O campo deve ser numérico");
-				options.ModelBindingMessageProvider.SetValueMustNotBeNullAccessor(x => "Este campo precisa ser preenchido.");
-			}
-			);  
-
-			services.AddScoped<MeuDbContext>();
-			services.AddScoped<IProdutoRepository, ProdutoRepository>();
-			services.AddScoped<IFornecedorRepository, FornecedorRepository>();
-			services.AddScoped<IEnderecoRepository, EnderecoRepository>();
-			services.AddSingleton<IValidationAttributeAdapterProvider, MoedaValidationAttributeAdapterProvider>();
-			//services.AddMvc();
+			services.AddMvcConfiguration();
+			services.ResolveDependencies();
 		}
 
-		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
 		public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
 			if(env.IsDevelopment()) {
 				app.UseDeveloperExceptionPage();
 				app.UseDatabaseErrorPage();
 			} else {
 				app.UseExceptionHandler("/Home/Error");
-				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
 				app.UseHsts();
 			}
 			app.UseHttpsRedirection();
@@ -82,15 +66,7 @@ namespace DevIO.App
 			app.UseAuthentication();
 			app.UseAuthorization();
 
-			//Configuração de Cultura
-			var defaultCulture = new CultureInfo("pt-BR");
-			var localizationOptions = new RequestLocalizationOptions {
-				DefaultRequestCulture = new RequestCulture(defaultCulture),
-				SupportedCultures = new List<CultureInfo> { defaultCulture },
-				SupportedUICultures = new List<CultureInfo> { defaultCulture }
-			};
-			app.UseRequestLocalization(localizationOptions);
-			//Fim
+			app.UseGlobalizationConfig();
 
 			app.UseEndpoints(endpoints => {
 				endpoints.MapControllerRoute(
