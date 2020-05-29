40944d2b781a896ba3d0e70077ab26eb42b959c7
Anderson <anderson@prodwin.com.br>
2020-05-28 23:39:22 -0300

    14 - Validando as Entidades de Negócio
    14 - Validando as Entidades de Negócio,
    Implementado as classes de serviço para validações.

---
+++ MinhaAppMvcCompleta/src/DevIO.Business/Interfaces/IFornecedorService.cs
@@ -0,0 +1,14 @@
+﻿using DevIO.Business.Models;
+using System;
+using System.Threading.Tasks;
+
+namespace DevIO.Business.Interfaces
+{
+	interface IFornecedorService
+	{
+		Task Adicionar(Fornecedor fornecedor);
+		Task Atualizar(Fornecedor fornecedor);
+		Task Remover(Guid id);
+		Task AtualizarEndereco(Endereco endereco);
+	}
+}
